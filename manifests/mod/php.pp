ea5bd6c9803330bf59eca74abd85d0ea90870824
Aaron Russo <aaron.n.russo@gmail.com>
2013-06-15 04:49:14 -0700

    adding support for php

---
+++ manifests/mod/php.pp
@@ -0,0 +1,16 @@
+class apache::mod::php {
+  file { '/etc/httpd/conf.d/php.conf':
+    ensure => present,
+    owner => root,
+    group => root,
+    mode => '0444',
+    replace => false,
+    source => 'puppet:///modules/apache/php.conf',
+    notify => Class['apache::service'],
+  }
+
+  file { '/etc/httpd/conf.d/module-php.conf':
+    ensure => link,
+    target => '/etc/httpd/conf.d/php.conf',
+  }
+}
