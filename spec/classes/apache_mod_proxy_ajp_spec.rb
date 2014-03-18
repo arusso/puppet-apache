94e01b9e3909cb8e91a43fd8fffd55e2d8d56406
Aaron Russo <aaron.n.russo@gmail.com>
2014-03-17 18:13:11 -0700

    add spec for apache::mod::proxy_ajp

---
+++ spec/classes/apache_mod_proxy_ajp_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe( 'apache::mod::proxy_ajp', :type => :class ) do
+  let(:redhat_facts) {{ 'osfamily' => 'RedHat' }}
+  let(:redhat6_facts) {{'operatingsystemrelease' => '6.5' }.merge(redhat_facts)}
+  let(:redhat5_facts) {{'operatingsystemrelease' => '5.10'}.merge(redhat_facts)}
+
+  context "on Red Hat" do
+    context "Version 5" do
+      let(:facts) {redhat5_facts}
+      it do
+        should contain_file('/etc/httpd/conf.d/proxy_ajp.conf').with({
+          'ensure' => 'file',
+          'owner' => 'root',
+          'group' => 'root',
+          'mode' => '0444',
+          'source' => 'puppet:///apache/proxy_ajp.conf',
+        })
+        should contain_file('/etc/httpd/conf.d/module-proxy_ajp.conf').with({
+          'ensure' => 'link',
+          'target' => '/etc/httpd/conf.d/proxy_ajp.conf',
+        })
+      end
+    end
+    context "Version 6" do
+      let(:facts) {redhat6_facts}
+      it do
+        should contain_file('/etc/httpd/conf.d/proxy_ajp.conf').with({
+          'ensure' => 'file',
+          'owner' => 'root',
+          'group' => 'root',
+          'mode' => '0444',
+          'source' => 'puppet:///apache/proxy_ajp.conf',
+        })
+        should contain_file('/etc/httpd/conf.d/module-proxy_ajp.conf').with({
+          'ensure' => 'link',
+          'target' => '/etc/httpd/conf.d/proxy_ajp.conf',
+        })
+      end
+    end
+  end
+end
