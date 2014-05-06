1566b34161d0e21aaf7b3b995ec2a38f48807063
Aaron Russo <arusso@berkeley.edu>
2014-05-06 14:32:55 -0700

    (maint) add spec tests for new template/source functionality

--- spec/defines/apache_vhost_spec.rb
+++ spec/defines/apache_vhost_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe( 'apache::vhost',:type => :define) do
   let :pre_condition do
-    'include apache'
+    'require ::apache::params'
   end
 
   let :default_facts do
@@ -37,6 +37,62 @@ describe( 'apache::vhost',:type => :define) do
         'group' => 'root',
         'mode' => '0444',
       })
+    end
+  end
+
+  context 'use a custom template' do
+    let(:title) { 'www.example.com' }
+    let(:facts) { redhat5_facts }
+    let :params do
+      {
+        'vhost_conf_template' => 'apache/test-only.erb',
+        'vhost_conf_source' => 'puppet:///modules/apache/some/other/file.conf',
+      }
+    end
+
+    it do
+      should contain_file('/etc/httpd/conf.d/vhost-1-www.example.com.conf').with({
+        :source => nil,
+        :content => /pain/,
+      })
+    end
+  end
+
+  context 'use a custom source' do
+    let(:title) { 'www.example.com' }
+    let(:facts) { redhat5_facts }
+    let :params do
+      {
+        'vhost_conf_source' => 'puppet:///modules/apache/test-only.conf',
+      }
+    end
+
+    it do
+      should contain_file('/etc/httpd/conf.d/vhost-1-www.example.com.conf').with({
+        :source => 'puppet:///modules/apache/test-only.conf',
+      })
+    end
+  end
+
+  context 'do not provide an include file' do
+    let(:title) { 'www.example.com' }
+    let(:facts) { redhat5_facts }
+    let :params do
+      {
+        :provide_include => false,
+      }
+    end
+
+    it do
+      should_not contain_file('/etc/httpd/conf.d/vhost-www.example.com.include')
+    end
+  end
+
+  context 'default provide_include behavior' do
+    let(:title) { 'www.example.com' }
+    let(:facts) { redhat5_facts }
+    it do
+      should contain_file('/etc/httpd/conf.d/vhost-www.example.com.include')
     end
   end
 end
